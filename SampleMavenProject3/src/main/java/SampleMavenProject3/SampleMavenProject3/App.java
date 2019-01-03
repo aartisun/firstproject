6acc65bb7edb5833f835fe343e3bce5167d5fc44
suniltandon <skumar.tandon@gmail.com>
2019-01-02 23:39:39 -0600

    First commit

---
+++ SampleMavenProject3/src/main/java/SampleMavenProject3/SampleMavenProject3/App.java
@@ -0,0 +1,15 @@
+package SampleMavenProject3.SampleMavenProject3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+     //  com.tcs.samplemavenproject1.App.sayHello1();
+       com.tcs.samplemavenproject1.App.main(args);
+    }
+}
