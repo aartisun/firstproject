7d455fccd5951e22c2d083044eb1d974a4cb688a
suniltandon <skumar.tandon@gmail.com>
2019-01-03 00:02:16 -0600

    3rd commit

--- SampleMavenProject3/src/main/java/SampleMavenProject3/SampleMavenProject3/App.java
+++ SampleMavenProject3/src/main/java/SampleMavenProject3/SampleMavenProject3/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //Test
        com.tcs.samplemavenproject1.App.main(args);
     }
 }
